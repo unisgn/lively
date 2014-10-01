13e9a3e12d9739bd763ff2fcfe73c463e0293bf7
unisgn@gmail.com <unisgn@gmail.com>
2014-10-01 13:46:36 +0800

    m

---
+++ src/main/java/com/lively/repository/TreeEntityRepository.java
@@ -0,0 +1,11 @@
+package com.lively.repository;
+
+import org.springframework.data.repository.NoRepositoryBean;
+
+/**
+ * Created by franCiS on Oct 01, 2014.
+ */
+
+@NoRepositoryBean
+public interface TreeEntityRepository {
+}
